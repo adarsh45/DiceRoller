e9d2cae5192dee11a7939b19a2480f5aadd8e380
Adarsh <shete.adarsh@gmail.com>
2019-05-23 17:14:02 +0530

    add BG Sound

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.diceroller;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Random random = new Random();
-    Button rollBtn;
-    ImageButton firstDice, secondDice;
+    private Button rollBtn;
+    private ImageButton firstDice, secondDice;
+    MediaPlayer mp;
 
 
     @Override
@@ -23,68 +26,44 @@ public class MainActivity extends AppCompatActivity {
         rollBtn = findViewById(R.id.rollBtn);
         firstDice = findViewById(R.id.first);
         secondDice = findViewById(R.id.second);
+        mp = MediaPlayer.create(this, R.raw.dice_sound_new);
 
         rollBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rollFirstDice();
-                rollSecondDice();
+                int random_1 = random.nextInt(6) + 1;
+                rollDice(random_1, firstDice);
+
+                int random_2 = random.nextInt(6) + 1;
+                rollDice(random_2, secondDice);
+
+                mp.start();
             }
         });
 
     }
 
-    public void rollFirstDice(){
-        int firstDiceNumber = random.nextInt(6) + 1;
-
-        switch (firstDiceNumber){
+    public void rollDice(int randomNum, ImageButton imageButton){
+        switch (randomNum){
             case 1:
-                firstDice.setImageResource(R.drawable.group1);
+                imageButton.setImageResource(R.drawable.group1);
                 break;
             case 2:
-                firstDice.setImageResource(R.drawable.group2);
+                imageButton.setImageResource(R.drawable.group2);
                 break;
             case 3:
-                firstDice.setImageResource(R.drawable.group3);
+                imageButton.setImageResource(R.drawable.group3);
                 break;
             case 4:
-                firstDice.setImageResource(R.drawable.group4);
+                imageButton.setImageResource(R.drawable.group4);
                 break;
             case 5:
-                firstDice.setImageResource(R.drawable.group5);
+                imageButton.setImageResource(R.drawable.group5);
                 break;
             case 6:
-                firstDice.setImageResource(R.drawable.group6);
+                imageButton.setImageResource(R.drawable.group6);
                 break;
         }
-
-    }
-
-    public void rollSecondDice(){
-
-        int secondDiceNumber = random.nextInt(6) + 1;
-
-        switch (secondDiceNumber){
-            case 1:
-                secondDice.setImageResource(R.drawable.group1);
-                break;
-            case 2:
-                secondDice.setImageResource(R.drawable.group2);
-                break;
-            case 3:
-                secondDice.setImageResource(R.drawable.group3);
-                break;
-            case 4:
-                secondDice.setImageResource(R.drawable.group4);
-                break;
-            case 5:
-                secondDice.setImageResource(R.drawable.group5);
-                break;
-            case 6:
-                secondDice.setImageResource(R.drawable.group6);
-                break;
-        }
-
     }
 
 }
